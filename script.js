c426e325ace6072f787771494ecb5beb520ad851
Bas <bmjdekker@gmail.com>
2021-03-31 13:09:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+/* 
+const addTheWordCool = function (array) {
+  // add your code
+  array.push("cool");
+  return array;
+};
+
+console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+*/
+
+/*
+const amountOfElementsInArray = function(array) {
+    return array.length;
+  };
+  console.log(amountOfElementsInArray(["appels", "peren", "citroenen"]));
+*/
+
+/*
+const selectBelgiumFromBenelux = function(array) {
+    return array[0];
+  }
+  console.log(selectBelgiumFromBenelux(["Belgie", "Nederland", "Luxemburg"])); 
+*/
+
+/*
+const lastElementInArray = function(array) {
+    return array.pop();
+    }
+    console.log(lastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])); 
+*/
+
+//SLICE:
+
+/*
+const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+const impeachTrumpSlice = function(array) {
+return array.slice(1)
+}
+console.log(impeachTrumpSlice(presidents)); 
+
+// Of SPLICE:  Verwijder positie 1 - add 3 overige op die plek
+
+const impeachTrumpSplice = function(array) {
+    return array.splice(1, 3)
+    }
+console.log(impeachTrumpSplice(presidents));
+*/
+
+/*
+ const stringsTogether = function(array) {
+   return array.join (" ");
+ }
+ 
+ console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}']))
+ */
+
+/*
+const combineArrays = function(array1, array2){
+    return array1.concat(array2);
+  };
+  console.log(combineArrays([1,2,3], [4,5,6]));
+  */
